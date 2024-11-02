067f505b943d7822c066aed6dc50c173615b7fe0
Heath Provost <heath_provost@stuller.com>
2024-11-02 16:16:09 -0500

    added install.ps1

--- install.ps1
+++ install.ps1
@@ -1,16 +1,29 @@
-function Get-CommandPath {
-  param ( [string] $command )
+# you must be an administrator to run this script
+
+function Get-GitCommandPath {
+  $result = $null
   try {
-    $path = (Get-Command -ErrorAction Stop -Name $command).Source
-    return $path
+    $result = (Get-Command -ErrorAction Stop -Name 'git').Source
   }
-  catch {
-    return $null
-  }
+  catch {}
+  return $result
 }
 
-$git_bin = Get-CommandPath "git"
-$gcm_bin = Get-CommandPath "git-credential-manager"
+function Get-GcmCommandPath {
+  $result = $null
+  $git_bin = Get-GitCommandPath
+  if ( $git_bin -ne $null -and $git_bin -match '\\cmd\\git\.exe$' ) {
+    $gcm_bin = $git_bin -replace "\\cmd\\git\.exe$",'\\mingw64\\bin\\git-credential-manager.exe'
+    if ( Test-Path -Path $gcm_bin ) {
+      $result = $gcm_bin
+    }
+  }
+  catch {}
+  return $result
+}
+
+$git_bin = Get-GitCommandPath
+$gcm_bin = Get-GcmCommandPath
 
 Write-Output "$git_bin"
 Write-Output "$gcm_bin"
