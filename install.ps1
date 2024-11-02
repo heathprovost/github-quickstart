ccb8ba08ba47033411995ea01060e7794ab7e4c3
Heath Provost <heath_provost@stuller.com>
2024-11-02 15:29:04 -0500

    added install.ps1

--- install.ps1
+++ install.ps1
@@ -2,17 +2,12 @@ function Get-Command-Path {
   Param (
     [string]$command
   )
-  $orgEap = $ErrorActionPreference
-  $ErrorActionPreference = ‘stop’
   try {
     $path = (Get-Command $command).Source
     return $path
   }
   catch {
     return $null
-  }
-  finally {
-    $ErrorActionPreference = $orgEap
   }
 }
 
