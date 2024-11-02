3445d3197d1f1bd901eadeb3505f81307cc14043
Heath Provost <heath_provost@stuller.com>
2024-11-02 15:28:02 -0500

    added install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,23 @@
-$git_bin = (Get-Command git).Source
-$gcm_bin = (Get-Command git-credential-manager).Source
+function Get-Command-Path {
+  Param (
+    [string]$command
+  )
+  $orgEap = $ErrorActionPreference
+  $ErrorActionPreference = ‘stop’
+  try {
+    $path = (Get-Command $command).Source
+    return $path
+  }
+  catch {
+    return $null
+  }
+  finally {
+    $ErrorActionPreference = $orgEap
+  }
+}
+
+$git_bin = Get-Command-Path "git"
+$gcm_bin = Get-Command-Path "git-credential-manager"
 
 Write-Output "$git_bin"
 Write-Output "$gcm_bin"
